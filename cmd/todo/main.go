1693c1720cbfbd8e1af0f92d48932a78e10a08db
Felipe <104157442+justafewwords4@users.noreply.github.com>
2024-06-19 18:28:57 -0600

    new main.go

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,40 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/justafewwords4/todo_go"
 	"os"
-	"strings"
+	// "strings"
 )
 
 // hardcoding el nombre de archivo a usar
 const todoFileName = ".todo.json"
 
 func main() {
+	// parsing command line flags
+	task := flag.String("task", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	// define an item list
 	l := &todo.List{}
 
-	// usar el método Get para leer los items del archivo
+	// use the Get command to read ToDo items from file
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	// switch para seleccionar qué hacer de acuerdo a los parámetros
+	// decidir qué hacer basado en las banderas
 	switch {
-	case len(os.Args) == 1:
-		// se usó nombre del comando, listar las tareas
+	case *list:
+		// listar los items en el archivo todo
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
-	// concatenar los argumentos y agregarlos a la lista de todo's
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		// Agregar la tarea
-		l.Add(item)
-		// save the new list
+	case *complete > 0:
+		// completar el item dado
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		// salvar la nueva lista
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		// agregar la nueva tarea
+		l.Add(*task)
+		// salvar la nueva lista
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
